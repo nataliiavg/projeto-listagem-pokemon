a714bbcaeb907c3358e136a347f606d7b87debef
Natalia Graciano <nataliavgraciano@gmail.com>
2023-05-18 20:18:31 -0300

    Change index.html directory

--- src/js/index.js
+++ src/js/index.js
@@ -26,8 +26,8 @@ botaoAlterarTema.addEventListener("click", () => {
   body.classList.toggle("modo-escuro");
 
   if (modoEscuroEstaAtivo) {
-    imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/sun.png");
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
   } else {
-    imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/moon.png");
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
   }
 });
